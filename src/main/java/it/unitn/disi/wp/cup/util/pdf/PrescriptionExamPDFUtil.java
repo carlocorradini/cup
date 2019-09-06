5339caba32c192012fba4f134766e132cd5a67c3
AyoubTheTroller <ayoub50.as@gmail.com>
2019-09-06 18:58:24 +0200

    PDF improvements 0.3

--- src/main/java/it/unitn/disi/wp/cup/util/pdf/PrescriptionExamPDFUtil.java
+++ src/main/java/it/unitn/disi/wp/cup/util/pdf/PrescriptionExamPDFUtil.java
@@ -372,9 +372,10 @@ public final class PrescriptionExamPDFUtil {
                         contentStream.setFont(font, 11);
                         contentStream.setNonStrokingColor(Color.BLACK);
                         contentStream.newLineAtOffset(50, cont);
-                        contentStream.showText("• " + e.getName());
+                        contentStream.showText("• ");
                         contentStream.endText();
-                        cont -= 20;
+                        cont = longString(e.getName(),document,contentStream,60,cont,11,font,465,15);
+                        cont -= 5;
                     }
                 }
                 if (!prescriptionExam.getReport().getMedicines().isEmpty()) {
@@ -390,9 +391,10 @@ public final class PrescriptionExamPDFUtil {
                         contentStream.setFont(font, 9);
                         contentStream.setNonStrokingColor(Color.BLACK);
                         contentStream.newLineAtOffset(50, cont);
-                        contentStream.showText("• " + e.getName());
-                        contentStream.endText();
-                        cont -= 20;
+                        contentStream.showText("• ");
+                        contentStream.endText();;
+                        cont = longString(e.getName(),document,contentStream,60,cont,11,font,465,15);
+                        cont -= 5;
                     }
                 }
             } else {
@@ -424,39 +426,6 @@ public final class PrescriptionExamPDFUtil {
         } catch (IOException ex) {
             LOGGER.log(Level.SEVERE, "Unable to generatePDF a Prescription Exam PDF", ex);
         }
-    }
-
-    public static BufferedImage resizeImage(BufferedImage img, Integer Width, Integer Height) {
-        int type = img.getType() == 0 ? BufferedImage.TYPE_INT_ARGB : img.getType();
-        if (Width == 0) {
-            Width = img.getWidth();
-        }
-        if (Height == 0) {
-            Height = img.getHeight();
-        }
-        int fHeight = Height;
-        int fWidth = Width;
-        //Work out the resized width/height
-        if (img.getHeight() > Height || img.getWidth() > Width) {
-            fHeight = Height;
-            int wid = Width;
-            float sum = (float) img.getWidth() / (float) img.getHeight();
-            fWidth = Math.round(fHeight * sum);
-            if (fWidth > wid) {
-                //rezise again for the width this time
-                fHeight = Math.round(wid / sum);
-                fWidth = wid;
-            }
-        }
-        BufferedImage resizedImage = new BufferedImage(fWidth, fHeight, type);
-        Graphics2D g = resizedImage.createGraphics();
-        g.setComposite(AlphaComposite.Src);
-        g.setRenderingHint(RenderingHints.KEY_INTERPOLATION, RenderingHints.VALUE_INTERPOLATION_BILINEAR);
-        g.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-        g.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-        g.drawImage(img, 0, 0, fWidth, fHeight, null);
-        g.dispose();
-        return resizedImage;
     }
 
     private static float longString(String text, PDDocument document, PDPageContentStream contentStream, float startX, float startY, int fontSize, PDFont pdfFont,int width,int linespacing){
